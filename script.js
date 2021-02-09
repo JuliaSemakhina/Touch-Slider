1228495d74843a5604cd05621e02dc6b6845c607
Julia Semakhina <contremoi@yandex.ru>
2021-02-09 15:15:36 +0300

    final files

--- script.js
+++ script.js
@@ -1,22 +1,87 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let wrapper = document.getElementById('wrapper');
-    let topLayer = wrapper.querySelector('.top');
-    let handle = wrapper.querySelector('.handle');
-    let skew = 0;
-    let delta = 0;
+const slider = document.querySelector('.slider-container'),
+    slides = Array.from(document.querySelectorAll('.slide'));
+
+let isDragging = false;
+startPos = 0,
+    currentTranslate = 0,
+    prevTranslate = 0,
+    animationID = 0,
+    currentIndex = 0
+
+slides.forEach((slide, index) => {
+    const slideImage = slide.querySelector('img');
+    slideImage.addEventListener('dragstart', (e) => e.preventDefault());
+
+    //Touch events
+    slide.addEventListener('touchstart', touchStart(index));
+    slide.addEventListener('touchend', touchEnd);
+    slide.addEventListener('touchmove', touchMove);
+
+    //Mouse events
+    slide.addEventListener('mousedown', touchStart(index));
+    slide.addEventListener('mouseup', touchEnd);
+    slide.addEventListener('mouseleave', touchEnd);
+    slide.addEventListener('mousemove', touchMove);
+})
 
 
-    if (wrapper.className.indexOf('skewed') != -1) {
-        skew = 990;
+//Disable context menu
+window.oncontextmenu = function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+}
+
+function touchStart(index) {
+    return function(event) {
+        currentIndex = index;
+        startPos = getPositionX(event);
+
+        isDragging = true;
+
+        animationID = requestAnimationFrame(animation);
+        slider.classList.add('grabbing');
     }
+}
 
-    wrapper.addEventListener('mousemove', function(e) {
-        delta = (e.clientX - window.innerWidth / 2) * 0.5;
+function touchEnd() {
+    isDragging = false;
+    cancelAnimationFrame(animationID);
 
-        handle.style.left = e.clientX + delta + "px";
+    const moveBy = currentTranslate - prevTranslate;
+    if (moveBy < -100 && currentIndex < slides.length - 1)
+        currentIndex += 1
+    if (moveBy > 100 && currentIndex > 0)
+        currentIndex -= 1
 
-        topLayer.style.width = e.clientX + skew + delta + "px";
-    })
+    setPositionByIndex()
+    slider.classList.remove('grabbing');
+}
 
+function touchMove(event) {
+    if (isDragging) {
+        const currentPosition = getPositionX(event);
+        currentTranslate = prevTranslate + currentPosition - startPos;
+    }
+}
 
-});
+function getPositionX(event) {
+    return event.type.includes('mouse') ?
+        event.pageX :
+        event.touches[0].clientX;
+}
+
+function animation() {
+    setSliderPosition();
+    if (isDragging) requestAnimationFrame(animation);
+}
+
+function setSliderPosition() {
+    slider.style.transform = `translateX(${currentTranslate}px)`;
+}
+
+function setPositionByIndex() {
+    currentPosition = currentIndex * -window.innerWidth
+    prevTranslate = currentTranslate
+    setSliderPosition()
+}
